cb0d0e8a20e3427c85e47f9f61b1583bd148e0f5
Philipp Frenzel <philipp@frenzel.net>
2014-02-19 21:46:24 +0100

    add codeception tests

--- config/web.php
+++ config/web.php
@@ -4,7 +4,7 @@ $params = require(__DIR__ . '/params.php');
 $db = require(__DIR__ . '/db.php');
 
 $config = [
-	'id' => 'basic',
+	'id' => 'neighbordrop',
 	'basePath' => dirname(__DIR__),
 	'extensions' => require(__DIR__ . '/../vendor/yiisoft/extensions.php'),
 	'components' => [
