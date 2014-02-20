84c5be40e6c76dada878d7a69b5677539366b566
Philipp Frenzel <philipp@frenzel.net>
2014-02-20 18:23:58 +0100

    cleaning up

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -48,7 +48,7 @@ AppAsset::register($this);
 			NavBar::end();
 		?>
 
-		<div class="container">
+		<div class="container-fluid" id="intro1">
 			<?= Breadcrumbs::widget([
 				'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
 			]) ?>
