f3c51a85eda58bd7fe36bcced67c64f77b49dc4e
Philipp Frenzel <philipp@frenzel.net>
2014-05-14 21:18:32 +0200

    play around

--- views/site/index.php
+++ views/site/index.php
@@ -1,9 +1,43 @@
 <?php
+
+use dosamigos\chartjs\Chart;
+use kartik\widgets\Affix;
+
 /**
  * @var yii\web\View $this
  */
 $this->title = 'NeighborDrop - Help your neighbor with droping picking or droping stuff';
 ?>
+
+<div class="map-logo">
+<?= Chart::widget([
+    'type' => 'Bar',
+    'options' => [
+        'height' => 300,
+        'width'  => 300,
+     ],
+    'data' => [
+        'labels' => ["January", "February", "March", "April", "May", "June", "July"],
+        'datasets' => [
+            [
+                'fillColor' => "rgba(220,220,220,0.5)",
+                'strokeColor' => "rgba(220,220,220,1)",
+                'pointColor' => "rgba(220,220,220,1)",
+                'pointStrokeColor' => "#fff",
+                'data' => [65, 59, 90, 81, 56, 55, 40]
+            ],
+            [
+                'fillColor' => "rgba(151,187,205,0.5)",
+                'strokeColor' => "rgba(151,187,205,1)",
+                'pointColor' => "rgba(151,187,205,1)",
+                'pointStrokeColor' => "#fff",
+                'data' => [28, 48, 40, 19, 96, 27, 100]
+            ]
+        ]
+    ]
+]);
+?>
+</div>
 
 <div class="container-fluid">
   <div class="row">
@@ -20,16 +54,10 @@ $this->title = 'NeighborDrop - Help your neighbor with droping picking or dropin
 <div class="container">
   <div class="row">
     <div class="col-md-6">
-      <img src="img/intro_one.png" alt="Intro">
+
     </div>
     <div class="col-md-6">
-      <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-      </div>
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">LOIGN</a></p>
     </div>
   </div>
 </div>
