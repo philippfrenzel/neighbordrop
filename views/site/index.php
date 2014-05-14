46962f15f373215240764eda06cc93d8d02cc33d
Philipp Frenzel <philipp@frenzel.net>
2014-05-14 23:14:26 +0200

    fix

--- views/site/index.php
+++ views/site/index.php
@@ -15,24 +15,24 @@ $this->title = 'NeighborDrop - Help your neighbor with droping picking or dropin
     'type' => 'Bar',
     'options' => [
         'height' => 300,
-        'width'  => 300,
+        'width'  => 80,
      ],
     'data' => [
-        'labels' => ["J", "F", "M", "A", "M", "J", "J"],
+        'labels' => ["RS"],
         'datasets' => [
             [
                 'fillColor' => "rgba(220,220,220,0.5)",
                 'strokeColor' => "rgba(220,220,220,1)",
                 'pointColor' => "rgba(220,220,220,1)",
                 'pointStrokeColor' => "#fff",
-                'data' => [65, 59, 90, 81, 56, 55, 40]
+                'data' => [65]
             ],
             [
                 'fillColor' => "rgba(151,187,205,0.5)",
                 'strokeColor' => "rgba(151,187,205,1)",
                 'pointColor' => "rgba(151,187,205,1)",
                 'pointStrokeColor' => "#fff",
-                'data' => [28, 48, 40, 19, 96, 27, 100]
+                'data' => [28]
             ]
         ]
     ]
