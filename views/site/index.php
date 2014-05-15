cae9a7897e3235eccdf777dd0f9cbab6d6389e02
Philipp Frenzel <philipp@frenzel.net>
2014-05-15 18:45:59 +0200

    fix heading

--- views/site/index.php
+++ views/site/index.php
@@ -69,7 +69,7 @@ $this->title = 'NeighborDrop - Help your neighbor with droping picking or dropin
     </div>
     <div class="col-md-4">
       <p class="fg_dark">
-        <h4>The Idea</h4>
+        <h3>The Idea</h3>
         NeighborDrop is a plattform that allows users to register several "drops". A drop
         can be your <b>HOME</b>, <b>WORKPLACE</b> or another place where you regulary live.
         After you registered at the platform you add your drops to your profile and then
@@ -82,7 +82,7 @@ $this->title = 'NeighborDrop - Help your neighbor with droping picking or dropin
     </div>
     <div class="col-md-4">
       <p class="fg_dark">
-        <h4>Samples</h4>
+        <h3>Samples</h3>
         <b>SUPPORTREQUEST</b>s are categorized by the type of support you request, e.g. need something
         from the groceries. Or maybe you need someone to help you with the laundry or you are sick and
         can't walk to the pharmacy.
@@ -96,7 +96,7 @@ $this->title = 'NeighborDrop - Help your neighbor with droping picking or dropin
     </div>
     <div class="col-md-4">
       <p class="fg_dark">
-        <h4>Security</h4>
+        <h3>Security</h3>
         As money matters in real life and even as we are a social plattform, we need a backup
         for the services <b>NEIGHBOR</b>s deliver, which means, as you are gonna buy a liter of
         milk for another user, you'll have to spend e.g. 50 Cents. So as you spend the money,
@@ -110,12 +110,12 @@ $this->title = 'NeighborDrop - Help your neighbor with droping picking or dropin
     </div>
     <div class="col-md-4">
       <p class="fg_dark">
-        <h4>Supporters</h4>
+        <h3>Supporters</h3>
         As not only money matters to us, we keep a statistic of all the important and good
         thinks that are done by our community. While you decide to join, you can always see
         how much good you have done in a month. Each month we give an award to the top 3 
         neighbors to honor their social behaviour.
-        <h4>Login</h4>
+        <h3>Login</h3>
         <?= \dektrium\user\widgets\Connect::widget(); ?>
       </p>
     </div>
